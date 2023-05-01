84e5f950f3d95f03ffa17689917dff015b47b9de
Nick Rogers <rogers.a.nick@gmail.com>
2023-05-01 13:53:22 -0400

    fix(db): update with appropriate RLS condition

--- supabase/migrations/20230501015730_add_owning_column_to_workouts.sql
+++ supabase/migrations/20230501015730_add_owning_column_to_workouts.sql
@@ -4,6 +4,8 @@ ALTER TABLE workouts ADD column user_id uuid NOT NULL DEFAULT auth.uid();
 
 ALTER TABLE workouts ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY workouts_auth_insert ON workouts FOR INSERT USING ( auth.uid() = user_id );
+CREATE POLICY workouts_auth_insert ON workouts
+    FOR INSERT
+    WITH CHECK (auth.uid() = user_id);
 
 CREATE POLICY workouts_private ON workouts USING (user_id = auth.uid());
